639acd232134f9445a10055083b82474073065c1
Pedro Henrique <pedrogv133@gmail.com>
2024-05-04 12:29:54 -0300

    feat: intial commit

---
+++ src/router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddlewares');
+
+const router = express.Router();
+
+router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id', 
+tasksMiddleware.validateFieldStatus, 
+tasksMiddleware.validateFieldTitle, 
+tasksController.updateTask,
+);
+
+module.exports = router;
